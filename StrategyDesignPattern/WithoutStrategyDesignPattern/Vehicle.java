85ae2efd73b55cf9786e1fa9da288438db80563c
Manoj Kumar Behera <82621905+MANOJ1509-behera@users.noreply.github.com>
2024-09-25 17:45:20 +0530

    strategy Design Pattern

---
+++ StrategyDesignPattern/WithoutStrategyDesignPattern/Vehicle.java
@@ -0,0 +1,7 @@
+package com.java.designPattern.strategyDesignPattern.withoutStrategyDesignPattern;
+
+public class Vehicle {
+    public void Drive(){
+        System.out.println("I am in Normal Drive Method");
+    }
+}
